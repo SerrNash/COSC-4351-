704f038a12f6a2a1412c132ba3c56bb4222ccddf
Username <eduardorobles6791@gmail.com>
2020-12-01 16:20:15 -0600

    add routes for admin role and  fixed backgroun directory in ejs files

--- server.js
+++ server.js
@@ -20,6 +20,29 @@ app.post('/authentication',function(request,response){
     response.render('Authentication');
 });
 
+app.post('/admin_role',function(request,response){
+    console.log(request.body);
+    var first_letter_username = request.body.username[0];
+    if(first_letter_username == '1'){
+        response.render('Support');
+    }
+    else if(first_letter_username == '2'){
+        response.render('Finance');
+    }
+    else if(first_letter_username == '3'){
+        response.render('Sales');
+    }
+    else if(first_letter_username == '4'){
+        response.render('hrAdmin');
+    }
+    else if(first_letter_username == '5'){
+        response.render('techAdmin');
+    }
+    else{
+        response.redirect('/');
+    }
+});
+
 app.listen('5000');
 
 
